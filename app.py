d748a9d9dace7d1389ab0595a75810fb76cdea36
Sudheesh E <sudheeshedathala@gmail.com>
2022-05-25 10:50:15 +0530

    app.py updated

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "App is successfully deployed"
 
 
 if __name__ == "__main__":
