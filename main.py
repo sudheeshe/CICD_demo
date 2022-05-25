4d052373abb462fb7629498c7af1013059aeee20
Sudheesh E <sudheeshedathala@gmail.com>
2022-05-25 11:09:39 +0530

    main.py updated

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from flask import Flask, session, request, Response, jsonify
 import atexit
 import uuid
 import os
+
 app = Flask(__name__)
 
 
@@ -21,8 +22,5 @@ port = int(os.getenv("PORT", 5001))
 
 if __name__ == "__main__":
     host = '0.0.0.0'
-    #app.run()
     httpd = simple_server.make_server(host=host, port=port, app=app)
-    #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
-    # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
